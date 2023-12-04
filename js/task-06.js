dfd7131b752613a5d702c6bca580445ac25c8bef
Filip-Povidernyi <p.poviderniy@gmail.com>
2023-12-04 10:47:08 +0200

    hw_fixed

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const dataInput = document.querySelector('#validation-input');
 const validLen = Number(dataInput.dataset.length);
 
 dataInput.addEventListener('blur', () => {
-    if (validLen <= dataInput.value.length) {
+    if (validLen === dataInput.value.length) {
         dataInput.classList.remove('invalid')
         dataInput.classList.add('valid');
     } else {
--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,8 @@
 const fontSizeCtrl = document.querySelector('#font-size-control');
 const textElem = document.querySelector('#text');
-fontSizeCtrl.addEventListener('change', () => {
+fontSizeCtrl.value = fontSizeCtrl.min;
+
+fontSizeCtrl.addEventListener('input', () => {
     textElem.style.fontSize = String(fontSizeCtrl.value) + 'px';
 });
+
